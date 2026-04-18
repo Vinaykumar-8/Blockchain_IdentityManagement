e925cf217a30441c8452a08b75cb2d023beabc8e
Vinaykumar-8 <vinay17022005@gmail.com>
2026-04-18 20:01:31 +0530

    Implement signature verification function
    Added a function to verify ECDSA signatures using a public key.

--- blockchain.go
+++ blockchain.go
@@ -49,3 +49,16 @@ func (bc *Blockchain) AddBlock(data string) {
 		log.Panic(err)
 	}
 }
+
+func VerifySignature(publicKeyBytes []byte, data []byte, signature []byte) bool {
+	r := new(big.Int).SetBytes(signature[:len(signature)/2])
+	s := new(big.Int).SetBytes(signature[len(signature)/2:])
+
+	curve := elliptic.P256()
+	x := new(big.Int).SetBytes(publicKeyBytes[:len(publicKeyBytes)/2])
+	y := new(big.Int).SetBytes(publicKeyBytes[len(publicKeyBytes)/2:])
+	rawPubKey := ecdsa.PublicKey{Curve: curve, X: x, Y: y}
+
+	hash := sha256.Sum256(data)
+	return ecdsa.Verify(&rawPubKey, hash[:], r, s)
+}
